68fb158131e4cafffe5cd7f13c9fd0ceecb84bf0
ypyd <56852756+ypyd@users.noreply.github.com>
2022-05-01 17:45:22 +0800

    Update build_image_rt-ac66u_380.70_0_ttls.sh

--- build_image_rt-ac66u_380.70_0_ttls.sh
+++ build_image_rt-ac66u_380.70_0_ttls.sh
@@ -16,19 +16,19 @@ if [ $? != 0 ]; then
   echo "apt upgrade is failed. Please run this script again."
   exit 1
 fi
-sudo -S apt-get -y install libtool-bin cmake libproxy-dev uuid-dev liblzo2-dev autoconf automake bash bison \
+sudo -S apt -y install libtool-bin cmake libproxy-dev uuid-dev liblzo2-dev autoconf automake bash bison \
      bzip2 diffutils file flex m4 g++ gawk groff-base libncurses-dev libtool libslang2 make patch perl pkg-config shtool \
      subversion tar texinfo zlib1g zlib1g-dev git-core gettext libexpat1-dev libssl-dev cvs gperf unzip \
      python libxml-parser-perl gcc-multilib gconf-editor libxml2-dev g++-multilib gitk libncurses5 mtd-utils \
      libncurses5-dev libvorbis-dev git autopoint autogen sed build-essential intltool libelf1:i386 libglib2.0-dev \
      xutils-dev lib32z1-dev lib32stdc++6 xsltproc gtk-doc-tools
 if [ $? != 0 ]; then
-  echo "apt get is failed. Please run this script again."
+  echo "apt install is failed. Please run this script again."
   exit 1
 fi
-sudo -S apt-get -y install lib32z1-dev lib32stdc++6
+sudo -S apt -y install lib32z1-dev lib32stdc++6
 if [ $? != 0 ]; then
-  echo "apt get is failed. Please run this script again."
+  echo "apt install is failed. Please run this script again."
   exit 1
 fi
 
